61293a12c699ef471de93bb502b371be96b28c80
cjpugs <cjcosta04@gmail.com>
2025-08-04 23:06:40 -0400

    buttery smooth navigation

--- transition.js
+++ transition.js
@@ -1,17 +1,29 @@
 document.querySelectorAll("a.fade-link").forEach(link => {
-            link.addEventListener("click", function(e) {
-                e.preventDefault();
-                const overlay = document.getElementById("transition-screen");
-                const href = this.getAttribute("href");
+    link.addEventListener("click", function(e) {
+        e.preventDefault();
+        const overlay = document.getElementById("transition-screen");
+        const href = this.getAttribute("href");
 
-                overlay.classList.add("active");
-                setTimeout(() => {
-                    window.location.href = href; // navigate after animation has played
-                }, 300); //match the time to the length of the animation
-            });
-        });
+        overlay.classList.add("active");
+        setTimeout(() => {
+            window.location.href = href; // navigate after animation has played
+        }, 300); //match the time to the length of the animation
+    });
+});
 
 window.addEventListener("load", () => {
     const overlay = document.getElementById("transition-screen");
     overlay.classList.remove("active");
+});
+
+document.querySelectorAll("a.navlink").forEach(link =>{
+    link.addEventListener("click", function(e){
+        const clicked = e.target;
+        
+        const selectedElement = document.getElementsByClassName("navlink-selected")[0];
+        selectedElement.classList.remove("navlink-selected");
+        selectedElement.classList.add("navlink");
+        
+        clicked.classList.add("navlink-selected");
+    });
 });
