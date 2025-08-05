91c0f71ad60d51af80aad63f28302c78dee5705a
cjpugs <cjcosta04@gmail.com>
2025-08-04 23:21:37 -0400

    even MORE butter

--- transition.js
+++ transition.js
@@ -4,6 +4,21 @@ document.querySelectorAll("a.fade-link").forEach(link => {
         const overlay = document.getElementById("transition-screen");
         const href = this.getAttribute("href");
 
+        const clicked = e.target;
+
+        const selectedElement = document.getElementsByClassName("navlink-selected")[0];
+        selectedElement.classList.remove("navlink-selected");
+        selectedElement.classList.add("navlink");
+
+        if (clicked === document.getElementById("header-name-link")){
+            console.log("home selected via title");
+            document.getElementById("navlink-home").classList.add("navlink-selected");
+        } else {
+            console.log("navigating to new page");
+            clicked.classList.add("navlink-selected");
+        }
+
+
         overlay.classList.add("active");
         setTimeout(() => {
             window.location.href = href; // navigate after animation has played
@@ -19,11 +34,7 @@ window.addEventListener("load", () => {
 document.querySelectorAll("a.navlink").forEach(link =>{
     link.addEventListener("click", function(e){
         const clicked = e.target;
+
         
-        const selectedElement = document.getElementsByClassName("navlink-selected")[0];
-        selectedElement.classList.remove("navlink-selected");
-        selectedElement.classList.add("navlink");
-        
-        clicked.classList.add("navlink-selected");
     });
 });
